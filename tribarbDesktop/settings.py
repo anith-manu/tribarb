452bc0f410d6dd16a03c987eb47183f7b9587fb7
anith-manu <anith.ravin@gmail.com>
2020-10-05 23:41:50 +0400

    secret key removed

--- tribarbDesktop/settings.py
+++ tribarbDesktop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xm_$3@0jrq*x6!_d1i0go)dq=@h2(1tdjy@593ioi$y9696-=5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
