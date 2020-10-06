d3a964ddf441e7b1b3e9c295dbc0b3f8550a310e
anith-manu <anith.ravin@gmail.com>
2020-10-06 11:45:23 +0400

    settings

--- tribarbDesktop/settings.py
+++ tribarbDesktop/settings.py
@@ -15,9 +15,9 @@ from pathlib import Path
 import os
 from django.contrib import messages
 
-if 'HEROKU' in os.environ:
-    import django_heroku
-    django_heroku.settings(locals())
+
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,4 +150,4 @@ LOGIN_REDIRECT_URL='home'
 LOGOUT_URL='logout'
 LOGOUT_REDIRECT_URL='login'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
