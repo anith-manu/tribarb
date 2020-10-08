f901058a85382a0765f5a77068b2c8f5097687fd
anith-manu <anith.ravin@gmail.com>
2020-10-08 23:31:58 +0400

    again

--- authentication/models.py
+++ authentication/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Shop(models.Model):
+	temp_id = models.UUIDField(default=uuid.uuid4)
 	#id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='shop')
 	name = models.CharField(max_length=500, blank=False)
